06697b3ba9c452fa5211e297e6fe3d1e7d5944c8
Riccardo Marini <riccardo.marini65@gmail.com>
2021-01-02 19:07:20 +0100

    wip

--- godot_proj/Client/Client.gd
+++ godot_proj/Client/Client.gd
@@ -5,7 +5,7 @@ extends Node
 # var a = 2
 # var b = "text"
 
-var client_app
+var client_app = preload("res://Client/ClientDll.gdns").new()
 
 const ClientApp_AppMode_InitialMenu = 0
 const ClientApp_AppMode_World = 1
@@ -14,12 +14,14 @@ const ClientApp_Login_NotDone = 0
 const ClientApp_Login_Started = 1
 const ClientApp_Login_Done = 2
 
-var time_from_last_message_pump = 0
-const Time_Period_Message_Pump = 0.001	# 1 millisecond
+var time_from_last_process = 0
+const Time_Period_Process = 0.001	# 1 millisecond
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print(self.name + ": _ready")
+	#client_app.say(ClientApp.hello("Test", "1", 2))
+	#add_child(ClientApp) # to activate TheWorld_GD_ClientApp::_ready & TheWorld_GD_ClientApp::_process
 	
 	#var root = get_tree().get_root()
 	#var current_scene = root.get_child(root.get_child_count() - 1)
@@ -29,11 +31,7 @@ func _ready():
 	#for n in child_nodes:
 	#	print(n.name)
 	
-	client_app = load("res://Client/ClientDll.gdns").new()
-	#client_app.say(ClientApp.hello("Test", "1", 2))
-	var result = connect("tree_exiting", self,"exitFunct",[])
-	
-	#add_child(ClientApp) # to activate TheWorld_GD_ClientApp::_ready & TheWorld_GD_ClientApp::_process
+	var result = connect("tree_exiting", self, "exitFunct",[])
 	
 	result = client_app.init()
 	client_app.set_app_mode(ClientApp_AppMode_InitialMenu, false)
@@ -42,16 +40,20 @@ func _ready():
 func _process(_delta):
 	#print(self.name + ": _process")
 	
-	var do_message_pump = false
-	time_from_last_message_pump += _delta
-	if time_from_last_message_pump > Time_Period_Message_Pump:
-		time_from_last_message_pump = 0
-		do_message_pump = true
-		
-	var do_sleep = client_app.get_do_sleep_in_main_loop()
-	if (!do_sleep || do_message_pump):
+	var do_process = false
+	time_from_last_process += _delta
+	if time_from_last_process > Time_Period_Process:
+		time_from_last_process = 0
+		do_process = true
+	
+	if !do_process:
+		pass
+
+	# Manage Server Event Message Pump
+	#var do_sleep = client_app.get_do_sleep_in_main_loop()
+	#if (!do_sleep):
 		#print(self.name + ": _process - call client_app.message_pump()")
-		client_app.message_pump()
+	client_app.message_pump()
 	
 func _init():
 	print("Client: _init")
--- godot_proj/UI.gd
+++ godot_proj/UI.gd
@@ -1,25 +1,73 @@
 extends Control
 
+const STATE_LOGIN = 0
+const STATE_AVATAR = 1
+const STATE_WORLD = 2
+var game_state
+var avatars_collected = false
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var time_from_last_process = 0
+const Time_Period_Process = 0.01	# 10 millisecond
 
+func set_game_state(state):
+	if state == STATE_LOGIN:
+		game_state = state
+		$LoginPanel.show()
+		$AvatarPanel.hide()
+	elif state == STATE_AVATAR:
+		game_state = state
+		avatars_collected = false
+		$AvatarPanel.hide()
+		$AvatarPanel.show()
+	#elif state == STATE_WORLD:
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-
+	var result = Client.client_app.connect("login_success", self, "login_success", [])
+	result = Client.client_app.connect("login_failed", self, "login_failed", [])
+	set_game_state(STATE_LOGIN)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(_delta):
+	var do_process = false
+	time_from_last_process += _delta
+	if time_from_last_process > Time_Period_Process:
+		time_from_last_process = 0
+		do_process = true
+	
+	if !do_process:
+		pass
+	
+	if game_state == STATE_AVATAR:
+		if !avatars_collected:
+			var avatar_count = Client.client_app.get_avatar_count()
+			if (avatar_count > 0):
+				avatars_collected = true
+				for i in range(0, avatar_count):
+					var id = Client.client_app.get_avatar_id(i)
+					var name = Client.client_app.get_avatar_name(i)
+					print("Avatar: (" + String(i) + ") - ID: " + String(id) + ", NAME: " + name)
+			
 
+	#if Client.client_app.get_app_mode() == ClientApp_AppMode_InitialMenu:
+		#if client_app.get_login_status() == ClientApp_Login_NotDone:
+		#elif client_app.get_login_status() == ClientApp_Login_Started:
+		#elif client_app.get_login_status() == ClientApp_Login_Done:
+	#elif Client.client_app.get_app_mode() == ClientApp_AppMode_World:
 
 func _on_loginButton_pressed():
-	Client.client_app.say("Login!")
-	var s = "TheWorldG"
+	var s = "TheWorld"
 	var ip = ""
 	var port = 0
+	$LoginPanel/Message.text = "Login in progress ..."
 	var result = Client.client_app.login($LoginPanel/usernameEdit.text, $LoginPanel/passwordEdit.text, s, ip, port)
 
+func login_success():
+	$LoginPanel/Message.text = "Login success"
+	set_game_state(STATE_AVATAR)
+	
+func login_failed(fail_code):
+	if fail_code == 20:
+		$LoginPanel/Message.text = "Login failed! Server is starting, please wait!"
+	else:
+		$LoginPanel/Message.text = "Login failed (error=" + String(fail_code) + ")!"
